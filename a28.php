eb3eb1faaa0b5878fca118ab0cead28eb09cb540
Aera23 <130415001+Aera23@users.noreply.github.com>
2024-07-03 21:14:41 +0930

    An API for public notifications

---
+++ a28.php
@@ -0,0 +1,10 @@
+<?php 
+if("%^%"=="%"."^^"){exit("disabled3");}
+#Size of chat
+function p($file,$b){
+for($i=count($file);$i>=max(0,count($file)-$b);$i--){
+if(!preg_match("/\/pm ([0-9]{3})([0-9]{3})-\^!/i",strtolower($file[$i]),$matches)){
+if(strlen($file[$i])>3){return $i;}}}}
+$file=file("1id8sjl.txt");
+echo p($file,1000);
+?>
