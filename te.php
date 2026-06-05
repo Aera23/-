4c9a81c2095dfdd30c76111e70e6d6fd931f44cd
Aera23 <130415001+Aera23@users.noreply.github.com>
2026-06-05 10:26:08 +0930

    defines $_SESSION['safe'] if missing
    so now nicks show again

--- te.php
+++ te.php
@@ -2,22 +2,23 @@
 $phrase=1;
 include_once('g3.php');
 $e="";
-$safe=str_replace(' ','_',substr($_SESSION['safe']??'',0,50));
+$safe=str_replace(' ','_',substr($_SESSION['safe']??npr($_REQUEST['nq'])??'',0,50));
+if(empty($_SESSION['safe'])){$_SESSION['safe']=$safe;}
 if(isset($_REQUEST['nq'])&&isset($_REQUEST['len'])){
 $col='<b style="color:'.htmlspecialchars($_COOKIE['col']??'#ddffdd').'">*</b>';
-if(file_exists("te.txt")){$messages=file("te.txt");}else{$messages=[''];}
-$count=count($messages);$message='';
-for($i=max(0,$count-4);$i<count($messages);$i++){$message.=$messages[$i];}
+if(file_exists("te.txt")){$events=file("te.txt");}else{$events=[''];}
+$count=count($events);$event='';
+for($i=max(0,$count-4);$i<count($events);$i++){$event.=$events[$i];}
 if(empty($_REQUEST['test'])){
 
 if(isset($_REQUEST['nq'])){$safe=$safe;}
 else{$safe='';}
 
 $nick=highlighter('@'.htmlspecialchars($safe));
-$message=preg_replace('|'.preg_quote($nick).' \[(.){1,3}\]|',str_replace('*','^'."\n",$col),$message);
-$message.=$nick.' ['.intval($_REQUEST['len'])."]\n ";}
-else{$message.=$col;$message=str_replace(htmlspecialchars($safe),'',$message);}
-file_put_contents("te.txt",preg_replace('|(<b style(.){9,}<\/b>){6}|',"\n",$message));
+$event=preg_replace('|'.preg_quote($nick).' \[(.){1,3}\]|',str_replace('*','^'."\n",$col),$event);
+$event.=$nick.' ['.intval($_REQUEST['len'])."]\n ";}
+else{$event.=$col;$event=str_replace(htmlspecialchars($safe),'',$event);}
+file_put_contents("te.txt",preg_replace('|(<b style(.){9,}<\/b>){6}|',"\n",$event));
 }
 if(file_exists("te.txt")){
 $events=file("te.txt");
